c2e830180d5b7be31bf62cb30784a32de108af07
danieta <31984540+danieta@users.noreply.github.com>
2019-05-19 12:37:04 +0100

    lagt til test og training i mapper, må få til validation også

---
+++ hw5.py
@@ -0,0 +1,18 @@
+# må sikkert ha flere imports her, tok alt fra hw3 bare
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import plot_model
+from keras.utils import to_categorical
+
+'''
+nyttige links?
+
+https://datascience.stackexchange.com/questions/15135/train-test-validation-set-splitting-in-sklearn
+https://www.geeksforgeeks.org/working-images-python/
+https://medium.com/nybles/create-your-first-image-recognition-classifier-using-cnn-keras-and-tensorflow-backend-6eaab98d14dd
+https://keras.io/preprocessing/image/
+'''
